bb10d167ad7cd39f6de1c092399c9b2287b137f3
brian <balsamo@uchicago.edu>
2018-02-05 11:41:06 -0600

    Threaded thumbnail requests, made next/previous look a bit prettier. Added a link to list view from thumbnail view, placated my linter a bit

--- iiifcollectionbrowse/blueprint/__init__.py
+++ iiifcollectionbrowse/blueprint/__init__.py
@@ -2,6 +2,7 @@
 iiifcollectionbrowse
 """
 import logging
+from threading import Thread
 
 from flask import Blueprint, render_template, url_for, request
 
@@ -22,6 +23,8 @@ BLUEPRINT = Blueprint('iiifcollectionbrowse', __name__,
 
 
 VIEWER_URL = "https://iiif-viewer.lib.uchicago.edu/uv/uv.html#"
+REQUESTS_TIMEOUT = 1/10
+NO_THUMB_IMG_URL = ""
 
 
 def get_thumbnail(rec, width=200, height=200, preserve_ratio=True):
@@ -31,19 +34,19 @@ def get_thumbnail(rec, width=200, height=200, preserve_ratio=True):
         width = "!"+str(width)
     if not isinstance(rec, dict):
         try:
-            resp = requests.get(rec)
+            resp = requests.get(rec, timeout=REQUESTS_TIMEOUT)
             resp.raise_for_status()
             rec = resp.json()
-        except:
-            # TODO: handle failure
+        except Exception:
+            # TODO: handle failure?
             raise
     # Try and update the record from its URI
     try:
-        remote_rec_resp = requests.get(rec['@id'])
+        remote_rec_resp = requests.get(rec['@id'], timeout=REQUESTS_TIMEOUT)
         remote_rec_resp.raise_for_status()
         remote_rec_json = remote_rec_resp.json()
         rec.update(remote_rec_json)
-    except:
+    except Exception:
         pass
     # If one is hardcoded
     if rec.get('thumbnail'):
@@ -86,9 +89,13 @@ def get_thumbnail(rec, width=200, height=200, preserve_ratio=True):
         raise ValueError()
 
 
+def threaded_thumbnails(identifier, result, index):
+    result[index] = get_thumbnail(identifier)
+
+
 @BLUEPRINT.route("/<path:c_url>")
 def collection(c_url):
-    resp = requests.get(c_url)
+    resp = requests.get(c_url, timeout=REQUESTS_TIMEOUT)
     resp.raise_for_status()
     rj = resp.json()
     members = []
@@ -97,7 +104,7 @@ def collection(c_url):
     page = request.args.get("page", 1)
     try:
         page = int(page)
-    except:
+    except Exception:
         page = 1
     if rj.get("members"):
         members = rj['members']
@@ -120,23 +127,35 @@ def collection(c_url):
         members = members[start:end]
         collections = collections[start:end]
         manifests = manifests[start:end]
+        if total > 10:
+            list_view = "/{}?page=-1".format(c_url)
         if end > total:
             next_page = None
         else:
             next_page = "/{}?page={}".format(c_url, str(page+1))
         prev_page = None
         if page > 1:
-            prev_page =  "/{}?page={}".format(c_url, str(page-1))
-        for x in members + collections + manifests:
-            try:
-                x['thumb_url'] = get_thumbnail(x)
-            except:
-                x['thumb_url'] = ""
+            prev_page = "/{}?page={}".format(c_url, str(page-1))
+        # https://stackoverflow.com/questions/6893968/
+        # how-to-get-the-return-value-from-a-thread-in-python
+        # Note: This makes the order the lists are concat'd together
+        # in important - otherwise thumbnails won't match.
+        results = [None] * len(members+collections+manifests)
+        for i, x in enumerate(members + collections + manifests):
+            x['thumb_thread'] = Thread(target=threaded_thumbnails, args=(x, results, i))
+            x['thumb_thread'].start()
+        for i, x in enumerate(members + collections + manifests):
+            x['thumb_thread'].join(timeout=10)
+            if results[i] is not None:
+                x['thumb_url'] = results[i]
+            else:
+                x['thumb_url'] = NO_THUMB_IMG_URL
         return render_template(
             "collection_individuals.html",
             viewer_url=VIEWER_URL,
             cname=rj['label'],
             cdesc=rj.get('description'),
+            list_view=list_view,
             next_page=next_page,
             prev_page=prev_page,
             members=members,
